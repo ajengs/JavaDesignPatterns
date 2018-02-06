0ba9919f81af24fce5d0ecf0a8643b3311ed3cb9
Ajeng <ajengs_92@yahoo.com>
2018-02-06 13:00:31 +0700

    Observer pattern (multi observers)

---
+++ src/observer/pattern/demo/Subject.java
@@ -0,0 +1,35 @@
+package observer.pattern.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject implements ISubject {
+	List<IObserver> observerList = new ArrayList<IObserver>();
+	private int myValue;
+	
+	public int getMyValue() {
+		return myValue;
+	}
+	
+	public void setMyValue(int myValue) {
+		this.myValue = myValue;
+		notifyObservers(myValue);
+	}
+	
+	@Override
+	public void register(IObserver o) {
+		observerList.add(o);
+	}
+
+	@Override
+	public void unregister(IObserver o) {
+		observerList.remove(o);
+	}
+
+	@Override
+	public void notifyObservers(int updatedValue) {
+		for(int i = 0; i < observerList.size(); i++) {
+			observerList.get(i).update(updatedValue);
+		}
+	}
+}
