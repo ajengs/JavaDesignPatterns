a5110baca25ef3657acc5f51eec9ef4c435704e7
Ajeng <ajengs_92@yahoo.com>
2018-02-06 15:28:54 +0700

    Singleton pattern demo1

---
+++ src/singleton/pattern/demo/MakeACaptain.java
@@ -0,0 +1,18 @@
+package singleton.pattern.demo;
+
+public class MakeACaptain {
+	private static MakeACaptain _captain;
+	
+	private MakeACaptain() {}
+	
+	public static MakeACaptain getCaptain() {
+		if(_captain == null) {
+			_captain = new MakeACaptain();
+			System.out.println("New captain selected for our team");
+		} else {
+			System.out.println("You already have a captain for your team");
+			System.out.println("Send him for the toss");
+		}
+		return _captain;
+	}
+}
